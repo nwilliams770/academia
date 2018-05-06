acf04a47c030ec8924ae80eb6df825621622a4d8
Nicholas <nwilliams770@gmail.com>
2018-05-05 17:40:25 -0700

    add starter files

---
+++ Understand_Nodejs/Helper_Files/C6_LetsRunSomeJavascript/Starter/app.js
@@ -0,0 +1,12 @@
+// Your Javascript Code Goes Here
+
+// sidenote: console.log() was added into Node and kept semantically the same
+// run files: node FILENAME.js
+// you can run this straight from VS code and you can add breakpoints!
+
+
+
+
+
+
+// Note: The 'starter' folders that appear in this course's downloadable source code is here to give you a starting point to try out coding yourself. The 'finished' folder contains the code as it is when we finish the lecture. You can use the 'finished' folder to compare to your own code, or just examine the finished code.
